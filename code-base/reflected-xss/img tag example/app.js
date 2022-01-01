484eae0b51fa78008cafbecf633b633cded4028c
Yahya Emre Cetinkaya <=>
2022-01-01 17:51:38 +0100

    added reflected-xss example

--- code-base/reflected-xss/img tag example/app.js
+++ code-base/reflected-xss/img tag example/app.js
@@ -1,8 +1,9 @@
 const userMessages = [];
 
 const userMessageForm = document.querySelector("form");
+// list of the
 const userMessagesList = document.querySelector("ul");
-
+// render the image  and message on the message
 function renderMessages() {
   let messageItems = "";
   for (const message of userMessages) {
@@ -10,7 +11,8 @@ function renderMessages() {
       ${messageItems}
       <li class="message-item">
         <div class="message-image">
-          <img src="${message.image}" alt="${message.text}">
+        <!-- by giving malicious code piece, we can run out js script  -->
+        <img src="${message.image}"  alt="${message.text}">
         </div>
         <p>${message.text}</p>
       </li>
@@ -19,14 +21,43 @@ function renderMessages() {
 
   userMessagesList.innerHTML = messageItems;
 }
+// mimic the accessToken that is stored in the localStorage
+function addLocalStorage() {
+  localStorage.setItem("accessToken", "This is the access token");
+}
 
+// handle the submit of the form
 function formSubmitHandler(event) {
+  addLocalStorage();
+  // prevent the form from redirecting
   event.preventDefault();
+  //  get the message from input
   const userMessageInput = event.target.querySelector("textarea");
+  // get the url of an image from input field
   const messageImageInput = event.target.querySelector("input");
+
   const userMessage = userMessageInput.value;
   const imageUrl = messageImageInput.value;
 
+  if (!checkFields(userMessage, imageUrl)) {
+    return;
+  }
+  // create an object to send the data that is obtained from input fields
+  userMessages.push({
+    text: userMessage,
+    image: imageUrl,
+  });
+  cleanup(userMessageInput, messageImageInput);
+  renderMessages();
+}
+
+// clean the input fields
+function cleanup(userMessageInput, messageImageInput) {
+  userMessageInput.value = "";
+  messageImageInput.value = "";
+}
+// check if the input fields are empty
+function checkFields(userMessage, imageUrl) {
   if (
     !userMessage ||
     !imageUrl ||
@@ -34,18 +65,9 @@ function formSubmitHandler(event) {
     imageUrl.trim().length === 0
   ) {
     alert("Please insert a valid message and image.");
-    return;
+    return false;
   }
-
-  userMessages.push({
-    text: userMessage,
-    image: imageUrl,
-  });
-
-  userMessageInput.value = "";
-  messageImageInput.value = "";
-
-  renderMessages();
+  return true;
 }
-
+// Add an submit listener to the form
 userMessageForm.addEventListener("submit", formSubmitHandler);
--- code-base/reflected-xss/img tag example/readme.txt
+++ code-base/reflected-xss/img tag example/readme.txt
@@ -1,9 +1,20 @@
 In this example we can use xss by giving a onerror method to img tag.
 
 1) write a name to box your message.
-2) we are passing url coming from user directly to the src of the img tagg.
+2) we are passing url coming from user directly to the src of the img tag.
 This is a vulnerability, since as string we can add " and close the src attribute,later
 we can write our  custom function into the onerror attribute.
  -> write " onerror="alert('hacked')" in the image box and submit the form.
  It will show an alert box.
  We can also write any javascript code that we want.
+ " onerror="()=>{document.body.innerHtml=''}"
+
+
+ or wer can write more complicated script. In this example we mimic the accessToken. Access Tokens are used to grant Authentication to web services. 
+ Generally they are stored at local storage of the browser. We can reach that by this simple script. Of course since it is reflected xss attack, we can reach only
+ the access token of the logged in user.
+" onerror="(function(){console.log( {...localStorage });})()"
+
+ function printLocalStorage() {
+  alert({ ...localStorage });
+}
